4fa247ebd4f3b77a67e599d055222287d7c1a4ce
hady wahba <157915807+Hadywahba@users.noreply.github.com>
2025-12-23 15:00:35 +0200

    refactor:update header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,5 +1,8 @@
 'use client';
-import { headerMobileSidebar, headerSidebar } from '@/lib/constants/header-menus';
+import {
+  headerMobileSidebar,
+  headerSidebar,
+} from '@/lib/constants/header-menus';
 import Image from 'next/image';
 import Link from 'next/link';
 import React, { useState } from 'react';
@@ -9,74 +12,72 @@ export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-   
-    <main className="flex items-center justify-between px-10 pt-[1.0625rem]">
-      {/* Left section */}
-      <section className="py-1">
-        <Link href="/">
-          <Image
-            src="/assets/icons/logo.svg"
-            alt="logo"
-            width={20}
-            height={32}
-            priority
-          />
-        </Link>
-      </section>
+      <main className="flex items-center justify-between px-10 pt-[1.0625rem]">
+        {/* Left section */}
+        <section className="py-1">
+          <Link href="/">
+            <Image
+              src="/assets/icons/logo.svg"
+              alt="logo"
+              width={20}
+              height={32}
+              priority
+            />
+          </Link>
+        </section>
 
-      {/* Right Section */}
-      <section className="flex items-center justify-around gap-4 text-white">
-        <nav aria-label="Main menu" className="hidden lg:flex">
-          <ul className="flex items-center justify-center gap-7">
-            {headerSidebar.map((item) => (
-              <Link key={item.id} href="/">
-                <li
-                  className={`text-sm ${item.name?.toLowerCase() === 'try free' ? 'rounded-md border-[.0625rem] border-white px-[1.4375rem] pb-[.4063rem] pt-[.3438rem] font-normal leading-5 tracking-[.0625rem] hover:border-primary hover:bg-white hover:text-primary' : 'font-medium leading-[1.61rem] tracking-[.0563rem]'}`}
-                >
-                  {item.image && item.alt && (
-                    <Image
-                      src={item.image}
-                      alt={item.alt}
-                      width={20}
-                      height={32}
-                      priority
-                    />
-                  )}
+        {/* Right Section */}
+        <section className="flex items-center justify-around gap-4 text-white">
+          <nav aria-label="Main menu" className="hidden lg:flex">
+            <ul className="flex items-center justify-center gap-7">
+              {headerSidebar.map((item) => (
+                <Link key={item.id} href="/">
+                  <li
+                    className={`text-sm ${item.name?.toLowerCase() === 'try free' ? 'rounded-md border-[.0625rem] border-white px-[1.4375rem] pb-[.4063rem] pt-[.3438rem] font-normal leading-5 tracking-[.0625rem] hover:border-primary hover:bg-white hover:text-primary' : 'font-medium leading-[1.61rem] tracking-[.0563rem]'}`}
+                  >
+                    {item.image && item.alt && (
+                      <Image
+                        src={item.image}
+                        alt={item.alt}
+                        width={20}
+                        height={32}
+                        priority
+                      />
+                    )}
 
-                  {item.name}
-                </li>
-              </Link>
-            ))}
-          </ul>
-        </nav>
-      </section>
+                    {item.name}
+                  </li>
+                </Link>
+              ))}
+            </ul>
+          </nav>
+        </section>
 
-      {/* Burger button */}
-      <button
-        className="lg:hidden text-white"
-        onClick={() => setIsOpen(!isOpen)}
-        aria-label="Toggle menu"
-      >
-        <svg
-          className="h-6 w-6"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
+        {/* Burger button */}
+        <button
+          className="text-white lg:hidden"
+          onClick={() => setIsOpen(!isOpen)}
+          aria-label="Toggle menu"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M4 6h16M4 12h16M4 18h16"
-          />
-        </svg>
-      </button>
-
-    </main>
-     {/* Mobile sliding menu */}
+          <svg
+            className="h-6 w-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+      </main>
+      {/* Mobile sliding menu */}
       <nav
         aria-label="Mobile menu"
-        className={`fixed top-0 right-0 z-50 h-full w-64 transform bg-white p-6 shadow-lg transition-transform duration-300 ease-in-out lg:hidden ${isOpen ? 'translate-x-full' : 'translate-x-0'}`}
+        className={`fixed right-0 top-0 z-50 h-full w-64 transform bg-white p-6 shadow-lg transition-transform duration-300 ease-in-out lg:hidden ${isOpen ? 'translate-x-full' : 'translate-x-0'}`}
       >
         <button
           className="mb-4 flex w-full justify-end"
@@ -99,12 +100,16 @@ export default function Header() {
         </button>
         <ul className="flex flex-col gap-6">
           {headerMobileSidebar.map((item) => (
-            <li key={item.id} className="w-full capitalize hover:underline">
-             {item.link ? (<Link href={item.link}>{item.name}</Link>) :item.name } 
+            <li key={item.id} className="w-full capitalize">
+              {item.link ? (
+                <Link href={item.link}>{item.name}</Link>
+              ) : (
+                item.name
+              )}
             </li>
           ))}
         </ul>
       </nav>
-     </>
+    </>
   );
 }
--- src/lib/constants/header-menus.ts
+++ src/lib/constants/header-menus.ts
@@ -43,7 +43,7 @@ export const headerSidebar: headerSidebarMenu[] = headersidebarcolumnItems.map(
   }),
 );
 
-// mobile sidebsr
+// mobile sidebar
 const headermobliesidebarcolumnItems: Omit<headerSidebarMenu, 'id'>[] = [
   {
     name: 'How it works',
